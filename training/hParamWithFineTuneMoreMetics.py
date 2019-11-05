6ab25f1220f4508478c79d334597c3202ae9e707
BrianOfrim <bofrim@ualberta.ca>
2019-11-05 09:19:42 -0800

    added an experiment for more metric tracking

---
+++ training/hParamWithFineTuneMoreMetics.py
@@ -0,0 +1,195 @@
+import os
+from sys import exit
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorboard.plugins.hparams import api as hp
+import itertools as it
+
+IMAGE_SIZE = 224
+BATCH_SIZE = 32
+VAL_SPLIT = 0.2
+NUM_INITIAL_EPOCHS = 2
+NUM_FINE_TUNE_EPOCHS = 2
+#
+#HP_BATCH_SIZE = hp.HParam('batch_size', hp.Discrete([8, 16, 32]))
+#HP_DROPOUT = hp.HParam('dropout', hp.Discrete([0.0, 0.1, 0.2, 0.3, 0.4, 0.5]))
+#HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete(['adam','RMSprop']))
+#HP_BASE_LEARNING_RATE = hp.HParam('base_learning_rate', hp.Discrete([0.001, 0.0001]))
+#HP_FINE_TUNE = hp.HParam('do_fine_tune', hp.Discrete([True, False]))
+
+HP_BATCH_SIZE = hp.HParam('batch_size', hp.Discrete([8, 16]))
+HP_DROPOUT = hp.HParam('dropout', hp.Discrete([0.0, 0.1]))
+HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete(['adam']))
+HP_BASE_LEARNING_RATE = hp.HParam('base_learning_rate', hp.Discrete([0.001]))
+HP_FINE_TUNE = hp.HParam('do_fine_tune', hp.Discrete([True]))
+
+
+METRIC_ACCURACY = 'accuracy'
+
+HPARAMS = [
+    HP_BATCH_SIZE,
+    HP_DROPOUT,
+    HP_OPTIMIZER,
+    HP_BASE_LEARNING_RATE,
+    HP_FINE_TUNE,
+]
+
+METRICS = [
+    hp.Metric(
+        METRIC_ACCURACY,
+        display_name='Accuracy'
+    ),
+    hp.Metric(
+        "epoch_accuracy",
+        group="validation",
+        display_name="accuracy (val.)",
+    ),
+    hp.Metric(
+        "epoch_loss",
+        group="validation",
+        display_name="loss (val.)",
+    ),
+    hp.Metric(
+        "batch_accuracy",
+        group="train",
+        display_name="accuracy (train)",
+    ),
+    hp.Metric(
+        "batch_loss",
+        group="train",
+        display_name="loss (train)",
+    ),
+]
+
+base_dir = './images'
+
+log_dir = 'logs/hparam_tuning/%i/' % int(time.time())
+
+
+with tf.summary.create_file_writer(log_dir).as_default():
+  hp.hparams_config(
+    hparams=HPARAMS,
+    metrics=METRICS,
+  )
+
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=45,
+    horizontal_flip=True,
+    validation_split=VAL_SPLIT)
+
+IMG_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 3)
+
+def train_test_model(run_dir, hparams):
+
+    base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                                   include_top=False,
+                                                   weights='imagenet')
+    train_generator = datagen.flow_from_directory(
+        directory=base_dir,
+        target_size=(IMAGE_SIZE, IMAGE_SIZE),
+        batch_size=hparams[HP_BATCH_SIZE], 
+        shuffle=True,
+        subset='training')
+
+    val_generator = datagen.flow_from_directory(
+        directory=base_dir,
+        target_size=(IMAGE_SIZE, IMAGE_SIZE),
+        batch_size=hparams[HP_BATCH_SIZE], 
+        subset='validation')
+    
+
+    callback = tf.keras.callbacks.TensorBoard(
+        run_dir,
+        update_freq='epoch',
+        profile_batch=0,  # workaround for issue #2084
+    )
+
+    hparams_callback = hp.KerasCallback(run_dir, hparams)
+
+    model = tf.keras.Sequential([
+      base_model,
+      tf.keras.layers.Dropout(hparams[HP_DROPOUT]),
+      tf.keras.layers.GlobalAveragePooling2D(),
+      tf.keras.layers.Dense(len(train_generator.class_indices), activation='softmax')
+    ])
+
+    opt = None
+    if(hparams[HP_OPTIMIZER]=='adam'):
+        opt = tf.keras.optimizers.Adam(hparams[HP_BASE_LEARNING_RATE])
+    if(hparams[HP_OPTIMIZER]=='RMSprop'):
+        opt = tf.keras.optimizers.RMSprop(hparams[HP_BASE_LEARNING_RATE])
+
+    model.compile(
+      optimizer=opt,
+      loss='categorical_crossentropy',
+      metrics=['accuracy'])
+
+    callback_ft = tf.keras.callbacks.TensorBoard(
+        run_dir,
+        update_freq='epoch',
+        profile_batch=0,  # workaround for issue #2084
+    )
+
+    hparams_callback_ft = hp.KerasCallback(run_dir, hparams)
+
+
+
+    model.fit_generator(
+        train_generator,
+        epochs=NUM_INITIAL_EPOCHS,
+        validation_data=val_generator,
+        callbacks=[callback_ft, hparams_callback_ft])
+    
+    if(not hparams[HP_FINE_TUNE]):
+        return
+    
+    if(hparams[HP_OPTIMIZER]=='adam'):
+        opt = tf.keras.optimizers.Adam(hparams[HP_BASE_LEARNING_RATE]/10)
+    if(hparams[HP_OPTIMIZER]=='RMSprop'):
+        opt = tf.keras.optimizers.RMSprop(hparams[HP_BASE_LEARNING_RATE]/10)
+
+    base_model.trainable = True
+    fine_tune_at = 100
+
+    # Freeze all the layers before the `fine_tune_at` layer
+    for layer in base_model.layers[:fine_tune_at]:
+        layer.trainable =  False
+
+    model.compile(
+      optimizer=opt,
+      loss='categorical_crossentropy',
+      metrics=['accuracy'])
+
+    model.fit_generator(
+        train_generator,
+        epochs=NUM_INITIAL_EPOCHS+NUM_FINE_TUNE_EPOCHS,
+        initial_epoch=NUM_INITIAL_EPOCHS,
+        validation_data=val_generator,
+        callbacks=[callback, hparams_callback])
+ 
+
+def run(run_dir, hparams):
+    train_test_model(run_dir, hparams)
+
+session_num = 0
+
+for batch_size in HP_BATCH_SIZE.domain.values:
+    for dropout in HP_DROPOUT.domain.values:
+        for optimizer in HP_OPTIMIZER.domain.values:
+            for base_learning_rate in HP_BASE_LEARNING_RATE.domain.values:
+                for fine_tune in HP_FINE_TUNE.domain.values:
+                    hparams = {
+                        HP_BATCH_SIZE: batch_size,
+                        HP_DROPOUT: dropout,
+                        HP_OPTIMIZER: optimizer,
+                        HP_BASE_LEARNING_RATE: base_learning_rate,
+                        HP_FINE_TUNE: fine_tune,
+                    }
+                    run_name = "run-%d" % session_num
+                    print('--- Starting trial: %s' % run_name)
+                    print({h.name: hparams[h] for h in hparams})
+                    run( log_dir + run_name, hparams)
+                    session_num += 1
