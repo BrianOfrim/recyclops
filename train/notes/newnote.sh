f90d08273dc9f29a25bfea5edd16bec3f995938f
BrianOfrim <bofrim@ualberta.ca>
2019-11-22 11:12:15 -0800

    Updated note

--- train/notes/newnote.sh
+++ train/notes/newnote.sh
@@ -1,5 +1,5 @@
-LATEST_TIMESTAMP=$(ls -1 -r ../logs/hparam_tuning/)
-NEW_FILE="${LATEST_TIMESTAMP}-notes.txt"
+LATEST_TIMESTAMP=$(ls -1 -r ../logs/hparam_tuning/ | head -1)
+NEW_FILE="${LATEST_TIMESTAMP}_notes.txt"
 touch $NEW_FILE
 echo $LATEST_TIMESTAMP >>  $NEW_FILE
 echo "batch_size\tdropout\toptimizer\tbase_learning_rate\tdo_file_tune\tacc(Val)\tloss(val)\tacc(train)\tloss(train)" >> $NEW_FILE
